5ca4d57fe8e1f49d382b275b01698604e5c896ec
fsa <1316240582@qq.com>
2022-07-19 13:50:06 +0800

    builder

---
+++ creational/builder.py
@@ -0,0 +1,19 @@
+# 
+# build by step in super __init__
+class builder():
+    def __init__(self) -> None:
+        self.buildStep1()
+        self.buildStep2()
+    def buildStep1(self):
+        raise NotImplemented
+    def buildStep2(self):
+        raise NotImplemented
+
+class myBuilder(builder):
+    def buildStep1(self):
+        print("step1")
+    def buildStep2(self):
+        print("step2")
+
+if __name__ == "__main__":
+    myBuilder()
