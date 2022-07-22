88f7ab6ad83eb2f6b9025410defc7094d9c149e1
fsa <1316240582@qq.com>
2022-07-22 14:50:51 +0800

    add iterator by python protocol

--- behavioral/iterator.py
+++ behavioral/iterator.py
@@ -1,3 +1,5 @@
+# type 1
+# create a new Iterator
 class Iterator():
     def __init__(self,elements) -> None:
         self.elements = elements
@@ -17,17 +19,42 @@ class NpcIterator(Iterator):
         return self.p<len(self.elements)-1
 
 class Aggregate():
-    def __init__(self,list) -> None:
-        self.list = list
+    def __init__(self,content) -> None:
+        self.content = content
     def createIterator():
         raise NotImplementedError
 
 class NpcAggregate(Aggregate):
     def createIterator(self):
-        return NpcIterator(self.list)
+        return NpcIterator(self.content)
+
+# type 2
+# using python iterator protocol
+
+class IterableNpcs():
+    def __init__(self,num) -> None:
+        self.npcs = []
+        self.p = -1
+        for i in range(num):
+            self.npcs.append("npc"+str(i))
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.p < len(self.npcs)-1:
+            self.p += 1
+            return self.npcs[self.p]
+        else:
+            raise StopIteration
 
 if __name__ == "__main__":
-    npcs = ["mike","jordan","han"]
+    
+    npcs = ("mike","jordan","han")
     iter = NpcAggregate(npcs).createIterator()
     while iter.hasNext():
         print(iter.next())
+
+    for npc in IterableNpcs(5):
+        print(npc)
+
