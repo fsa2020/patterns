44726f70446f2b091c2e943278b50f3c43b40529
fsa <1316240582@qq.com>
2022-07-22 16:01:49 +0800

    mediator

---
+++ behavioral/mediator.py
@@ -0,0 +1,38 @@
+class Mediator():
+    def __init__(self) -> None:
+        self.player = Player(self)
+        self.npcList = [Npc(self),Npc(self),Npc(self)]
+
+    def invoke(self,event):
+        if event == "PlayerSpeak":
+            for npc in self.npcList:
+                npc.Listen()
+        elif event == "NpcSpeak":
+             self.player.Listen()
+        # add event type
+        else:
+            return
+
+class Actor():
+    def __init__(self,mediator) -> None:
+        self.mediator = mediator
+
+class Player(Actor):
+    def speak(self):
+        print("player speak")
+        self.mediator.invoke("PlayerSpeak")
+    def Listen(self):
+        print("player listening")
+    
+class Npc(Actor):
+    def speak(self):
+        print("npc speak")
+        self.mediator.invoke("NpcSpeak")
+    def Listen(self):
+        print("npc listening")
+
+if __name__ == "__main__":
+    mediator = Mediator()
+    mediator.player.speak()
+    mediator.npcList[0].speak()
+    
